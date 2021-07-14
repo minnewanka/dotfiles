f19973a32641fd75cda74896c4e0ed2fd13c49c4
minnewanka <philippe.i@swingvy.com>
2021-07-14 21:52:21 +0900

    clean

--- nvim/lua/highlight.lua
+++ nvim/lua/highlight.lua
@@ -1,8 +1,6 @@
 vim.api.nvim_exec([[
 function! MyHighlights() abort
-hi! link GitSignsCurrentLineBlame Comment
 highlight link YankHighlight Visual
-highlight Comment cterm=italic gui=italic
 highlight link TelescopeSelection TSTextReference
 highlight link TelescopeSelectionCaret TSTextReference
 endfunction
