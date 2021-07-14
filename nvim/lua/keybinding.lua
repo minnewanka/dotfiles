642edea77fae79e72f44d20f9bf7903cf281b84f
minnewanka <philippe.i@swingvy.com>
2021-07-14 21:51:31 +0900

    clean keybinding

--- nvim/lua/keybinding.lua
+++ nvim/lua/keybinding.lua
@@ -1,12 +1,14 @@
 vim.g.mapleader = ' '
 
+--source file
+vim.api.nvim_set_keymap('n', '<leader>r', ':source %<CR>', { noremap = true, silent = false })
+
 -- selection
 vim.api.nvim_set_keymap('i', '<C-j>', '<C-n>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<C-k>', '<C-p>', { noremap = true })
 
 vim.api.nvim_set_keymap('c', '<C-k>', 'pumvisible() ? "<C-p>" : "<Up>"', { noremap = true, expr = true, silent = false})
 vim.api.nvim_set_keymap('c', '<C-j>', 'pumvisible() ? "<C-n>" : "<Down>"', { noremap = true, expr = true, silent = false})
-
 
 -- paste
 vim.api.nvim_set_keymap('v', '<leader>p', '"_dP', { noremap = true, silent = true })
@@ -64,27 +66,21 @@ vim.api.nvim_set_keymap('n', '^', '<C-^>', { noremap = true, silent = true })
 -- Go end of line in Insert Mode
 vim.api.nvim_set_keymap('i', '<C-e>', '<C-o>$', { noremap = true, silent = true })
 
--- terminal
-vim.api.nvim_set_keymap('t', 'jk', '<C-\\><C-n>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
-
 -- word wrap
 vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap=true, expr = true, silent = true})
 vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", {noremap= true, expr = true, silent = true})
 
 -- git diff
 vim.api.nvim_set_keymap('n', '<Leader>g',  ':ToggleGStatus<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>gd',  ':Git difftool<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', ';g;', ':diffget //3<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', ';gj', ':diffget //2<CR>', { noremap = true, silent = true })
 
 -- Tab
 vim.api.nvim_set_keymap('i', '<TAB>', "vsnip#available(1)?'<Plug>(vsnip-expand-or-jump)':'<Tab>'", { expr = true, silent = true})
-vim.api.nvim_set_keymap('n', 'gb', ':BufferLinePick<CR>', {noremap = true, silent = true})
 
 -- reselect map
 vim.api.nvim_set_keymap('n', 'gy', "'`[' . strpart(getregtype(), 0, 1) . '`]'", { noremap = true ,expr = true, silent = true})
-
-vim.api.nvim_set_keymap('n', 'gx', ":!open <c-r><c-a>", { expr = true, silent = true})
 
 --marks
 vim.api.nvim_set_keymap('n', '`', '\'', {noremap = true, silent = true})
@@ -93,12 +89,9 @@ vim.api.nvim_set_keymap('n', '\'', '`', {noremap = true, silent = true})
 -- folding
 vim.api.nvim_set_keymap('n', '<BS>', 'za', {noremap = true, silent = true})
 
-
 -- Telescope
 vim.api.nvim_set_keymap('n', '<C-P>',  [[<Cmd>Telescope git_files<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>b',  [[<Cmd>Telescope buffers<CR>]], { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<Leader>o',  [[<Cmd>lua require('telescope.builtin').oldfiles({cwd_only=true,include_current_session=true})<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>f',  [[<Cmd>Telescope live_grep<CR>]], { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', 'gr',  [[<Cmd>lua require('telescope.builtin').lsp_references()<CR>]], { noremap = true, silent = true })
 
 
