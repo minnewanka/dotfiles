6fefeb8d2816f841363ee1835ffcfd4d8de9e10b
minnewanka <philippe.inthavong@gmail.com>
2022-08-03 22:04:10 +0900

    clean plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -282,51 +282,10 @@ use({
         'jose-elias-alvarez/typescript.nvim',
     }
     use {'folke/trouble.nvim'}
-    --[[ use {
-        'anuvyklack/hydra.nvim',
-        config = function()
-            require('plugins.hydras.window')
-        end
-    }  ]]
-    -- use {'Pocco81/auto-save.nvim'}
     use {
         'mrjones2014/smart-splits.nvim',
         config = function()
             require('smart-splits').setup({});
-        end
-    }
-    use {
-        'sindrets/winshift.nvim',
-        config = function()
-            require("winshift").setup({
-                -- {other options...}
-
-                ---A function that should prompt the user to select a window.
-                ---
-                ---The window picker is used to select a window while swapping windows with
-                ---`:WinShift swap`.
-                ---@return integer? winid # Either the selected window ID, or `nil` to
-                ---   indicate that the user cancelled / gave an invalid selection.
-                window_picker = function()
-                    return require("winshift.lib").pick_window({
-                        -- A string of chars used as identifiers by the window picker.
-                        picker_chars = "BCD",
-                        filter_rules = {
-                            -- This table allows you to indicate to the window picker that a window
-                            -- should be ignored if its buffer matches any of the following criteria.
-                            cur_win = true, -- Filter out the current window
-                            floats = true,  -- Filter out floating windows
-                            filetype = {},  -- List of ignored file types
-                            buftype = {},   -- List of ignored buftypes
-                            bufname = {},   -- List of vim regex patterns matching ignored buffer names
-                        },
-                        ---A function used to filter the list of selectable windows.
-                        ---@param winids integer[] # The list of selectable window IDs.
-                        ---@return integer[] filtered # The filtered list of window IDs.
-                        filter_func = nil,
-                    })
-                end,
-            })
         end
     }
 
