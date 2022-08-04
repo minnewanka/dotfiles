ecf4778c642aee365e76f2810520f9f262ea30fc
minnewanka <philippe.i@swingvy.com>
2022-08-04 13:08:55 +0900

    update colorscheme

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -264,7 +264,7 @@ use({
     }
 
     use { 'ggandor/leap.nvim', event = "BufRead" , config = function() require("plugins.leap") end }
-    -- use { 'EdenEast/nightfox.nvim' }
+    use { 'EdenEast/nightfox.nvim' }
     use { 'j-hui/fidget.nvim', config = function() require"fidget".setup{
         sources = {
             ["null-ls"] = {
@@ -288,7 +288,6 @@ use({
             require('smart-splits').setup({});
         end
     }
-    use 'sainnhe/gruvbox-material'
 
 
 end )
