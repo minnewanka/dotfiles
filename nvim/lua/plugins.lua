a1e61a09b08996393dbfc1b6628264ab668875a6
minnewanka <philippe.i@swingvy.com>
2022-08-24 21:18:27 +0900

    update tmux/kitty config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -292,14 +292,6 @@ use({
     }
 
     use {
-        'mickael-menu/zk-nvim',
-        config = function()
-            require("zk").setup({
-                picker = "telescope",
-            })
-        end
-    }
-    use {
         'preservim/vim-markdown',
         config = function()
             vim.g.vim_markdown_conceal = 2
