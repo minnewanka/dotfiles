52eda4eecc7aa81a158da5823d8a03ae7e23ad76
minnewanka <philippe.inthavong@gmail.com>
2022-09-30 21:51:26 +0900

    clean comment

--- nvim/lua/lsp/server.lua
+++ nvim/lua/lsp/server.lua
@@ -4,13 +4,6 @@ local root_pattern = util.root_pattern
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
--- npm install -g typescript typescript-language-server
---[[ require'lspconfig'.tsserver.setup({
-    filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-    on_attach =  on_attach,
-    capabilities = capabilities
-}) ]]
-
 require("typescript").setup({
     disable_commands = false, -- prevent the plugin from creating Vim commands
     debug = false, -- enable debug logging for commands
