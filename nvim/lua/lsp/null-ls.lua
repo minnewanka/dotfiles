fd384c6e1b04bb29f0e64def29c2351bf0cc1e33
minnewanka <philippe.inthavong@gmail.com>
2022-10-16 01:35:23 +0900

    add format on save

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -8,5 +8,17 @@ null_ls.setup({
 		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.stylua,
 	},
-	on_attach = on_attach,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+		on_attach(client, bufnr)
+	end,
 })
