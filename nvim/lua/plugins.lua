2d036eee0157764bb87d187a92cbc1a18f7770c2
minnewanka <philippe.inthavong@gmail.com>
2025-10-07 23:43:23 +0900

    feat: add neotree

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -20,66 +20,6 @@ return {
 			vim.api.nvim_set_keymap("i", "<C-p>", "copilot#Previous()", { silent = true, expr = true })
 		end,
 	},
-	-- {
-	-- 	"nvim-mini/mini.nvim",
-	-- 	version = false,
-	-- 	config = function()
-	-- 		require("mini.files").setup()
-	-- 		vim.keymap.set("n", "<leader>fe", function()
-	-- 			local buf_name = vim.api.nvim_buf_get_name(0)
-	-- 			local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
-	-- 			MiniFiles.open(path)
-	-- 			MiniFiles.reveal_cwd()
-	-- 		end, { desc = "Open Mini Files" })
-	--
-	-- 		require("mini.ai").setup()
-	--
-	-- 		local miniclue = require("mini.clue")
-	-- 		miniclue.setup({
-	-- 			triggers = {
-	-- 				-- Leader triggers
-	-- 				{ mode = "n", keys = "<Leader>" },
-	-- 				{ mode = "x", keys = "<Leader>" },
-	--
-	-- 				-- Built-in completion
-	-- 				{ mode = "i", keys = "<C-x>" },
-	--
-	-- 				-- `g` key
-	-- 				{ mode = "n", keys = "g" },
-	-- 				{ mode = "x", keys = "g" },
-	--
-	-- 				-- Marks
-	-- 				{ mode = "n", keys = "'" },
-	-- 				{ mode = "n", keys = "`" },
-	-- 				{ mode = "x", keys = "'" },
-	-- 				{ mode = "x", keys = "`" },
-	--
-	-- 				-- Registers
-	-- 				{ mode = "n", keys = '"' },
-	-- 				{ mode = "x", keys = '"' },
-	-- 				{ mode = "i", keys = "<C-r>" },
-	-- 				{ mode = "c", keys = "<C-r>" },
-	--
-	-- 				-- Window commands
-	-- 				{ mode = "n", keys = "<C-w>" },
-	--
-	-- 				-- `z` key
-	-- 				{ mode = "n", keys = "z" },
-	-- 				{ mode = "x", keys = "z" },
-	-- 			},
-	--
-	-- 			clues = {
-	-- 				-- Enhance this by adding descriptions for <Leader> mapping groups
-	-- 				miniclue.gen_clues.builtin_completion(),
-	-- 				miniclue.gen_clues.g(),
-	-- 				miniclue.gen_clues.marks(),
-	-- 				miniclue.gen_clues.registers(),
-	-- 				miniclue.gen_clues.windows(),
-	-- 				miniclue.gen_clues.z(),
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
 	{
 		"stevearc/conform.nvim",
 		event = { "BufReadPre", "BufNewFile" },
@@ -749,6 +689,19 @@ return {
 		end,
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		lazy = false, -- neo-tree will lazily load itself
+		keys = {
+			{ "<leader>nt", "<cmd>Neotree toggle<cr>", desc = "Toggle Neotree" },
+		},
+	},
 
 	{
 		"norcalli/nvim-colorizer.lua",
@@ -899,9 +852,6 @@ return {
 	},
 	{
 		"saghen/blink.cmp",
-		dependencies = {
-			"Kaiser-Yang/blink-cmp-avante",
-		},
 		-- optional: provides snippets for the snippet source
 
 		-- use a release tag to download pre-built binaries
@@ -958,13 +908,7 @@ return {
 			-- Default list of enabled providers defined so that you can extend it
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
-				default = { "avante", "lsp", "path", "snippets", "buffer" },
-				providers = {
-					avante = {
-						module = "blink-cmp-avante",
-						name = "Avante",
-					},
-				},
+				default = { "lsp", "path", "snippets", "buffer" },
 			},
 
 			-- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
